6066e2351edbb043839b02b8c8a4588fefe248b0
Alexander Blair <git@jagtech.io>
2025-05-12 18:43:19 -0700

    feat(transaction): add transaction logging
    Add the ability to track a transaction log

---
+++ cmd/payoutDaemon/sql/transactions.go
@@ -0,0 +1,11 @@
+package sql
+
+import (
+	"context"
+	"github.com/jackc/pgx/v4"
+)
+
+func CreateNewTransaction(psqlTx pgx.Tx, txID uint64, success bool, errorString *string, balanceID uint64, batchID uint64) error {
+	_, err := psqlTx.Exec(context.Background(), "insert into transactions (id, success, error, balance_id, batch_id) values ($1, $2, $3, $4, $5)", txID, success, errorString, balanceID, batchID)
+	return err
+}
