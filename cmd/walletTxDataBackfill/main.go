0376178debb31877f0e33a4585f4ebd2a90ddcc9
Alexander Blair <git@jagtech.io>
2025-05-27 16:38:05 -0700

    feat(cmd): add txbackfill tool
    This scans the DB for completed transactions that are missing their
     extended transaction data and loads it into the database in order to
     properly scan for transactions that need to be re-allowed

---
+++ cmd/walletTxDataBackfill/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"github.com/Snipa22/core-go-lib/helpers"
+	core "github.com/Snipa22/core-go-lib/milieu"
+	"github.com/Snipa22/go-tari-faucet/cmd/payoutDaemon/sql"
+	"github.com/Snipa22/go-tari-grpc-lib/v2/walletGRPC"
+	"github.com/jackc/pgx/v4"
+)
+
+func main() {
+	psqlURL := helpers.GetEnv("PSQL_SERVER", "postgres://postgres@localhost/postgres?sslmode=disable")
+	sentryURI := helpers.GetEnv("SENTRY_SERVER", "")
+
+	// Build Milieu
+	milieu, err := core.NewMilieu(&psqlURL, nil, &sentryURI)
+	if err != nil {
+		milieu.CaptureException(err)
+		milieu.Fatal(err.Error())
+	}
+
+	walletGRPCAddressPtr := flag.String("wallet-grpc-address", "127.0.0.1:18143", "Tari wallet GRPC address")
+	flag.Parse()
+	walletGRPC.InitWalletGRPC(*walletGRPCAddressPtr)
+
+	rows, err := milieu.GetRawPGXPool().Query(context.Background(), "select id from transactions where success is true")
+	if err != nil {
+		milieu.CaptureException(err)
+		milieu.Fatal(err.Error())
+	}
+	defer rows.Close()
+	idList := make([]uint64, 0)
+	for rows.Next() {
+		id := new(uint64)
+		if err = rows.Scan(&id); err != nil {
+			milieu.CaptureException(err)
+			milieu.Info(err.Error())
+			continue
+		}
+		idList = append(idList, *id)
+	}
+
+	txnToBackfill := make([]uint64, 0)
+	for _, id := range idList {
+		row := milieu.GetRawPGXPool().QueryRow(context.Background(), "select id from transaction_details where id in ($1)", id)
+		idScan := new(uint64)
+		if err := row.Scan(&idScan); err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				txnToBackfill = append(txnToBackfill, *idScan)
+				continue
+			}
+			milieu.CaptureException(err)
+			milieu.Fatal(err.Error())
+		}
+	}
+
+	for _, id := range txnToBackfill {
+		txnData, err := walletGRPC.GetTransactionInfoByID(id)
+		if err != nil {
+			milieu.CaptureException(err)
+			milieu.Info(err.Error())
+			continue
+		}
+		if err = sql.CreateTransactionDetail(milieu, txnData); err != nil {
+			milieu.CaptureException(err)
+			milieu.Info(err.Error())
+			continue
+		}
+	}
+}
